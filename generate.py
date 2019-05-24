ecacd7f411dfe1fad0f1482202f772837d764b6f
lekhuong07 <38338056+lekhuong07@users.noreply.github.com>
2019-05-24 13:11:42 +0700

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,68 +1,128 @@
-from PIL import Image, ImageChops, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 import cv2, glob, os, requests
-import numpy as np
+import numpy
 
-def delete_background(path):
-    img = cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB)
-    lower_white = np.array([220, 220, 220], dtype=np.uint8)
-    upper_white = np.array([255, 255, 255], dtype=np.uint8)
-    mask = cv2.inRange(img, lower_white, upper_white)  # could also use threshold
-    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (
-    3, 3)))  # "erase" the small white points in the resulting mask
-    mask = cv2.bitwise_not(mask)  # invert mask
+def layer_on_bw(img, img2):
+    img = img.convert("RGBA")
+    data1 = img.getdata()
+    img2  = img2.convert("RGBA")
+    data2 = img2.getdata()
+    newData = []
+    # data 1 original
+    # data 2 original with new black background image
+    i = 0
+    for item in data2:
+        if item[0] == 0 and item[1] == 0 and item[2] == 0:
+            newData.append(data1[i])
+        else:
+           newData.append(item)
+        i += 1
+    img.putdata(newData)
+    return img
+'''
+* corner part is divided into 2 parts:
+* predict part 
+* purple thing
+'''
+def layer_on_topcorner(img, img2):
+    img = predict_part(img, img2)
+    img = purple_thing(img, img2)
+    return img
 
-    # load background (could be an image too)
-    bk = np.full(img.shape, 255, dtype=np.uint8)  # white bk
+def predict_part(img, img2):
+    img = img.convert("RGBA")
+    data1 = img.getdata()
+    img2 = img2.convert("RGBA")
+    data2 = img2.getdata()
+    newData = []
+    # data 1 is the layer below
+    # data 2 is the layer on top
+    i = 0
+    for item in data2:
+        if item[0] > 150 and item[1] > 150 and item[2] < 100:
+            newData.append(item)
+        else:
+            newData.append(data1[i])
+        i += 1
+    img.putdata(newData)
+    # img.show()
+    return img
 
-    # get masked foreground
-    fg_masked = cv2.bitwise_and(img, img, mask=mask)
+def purple_thing(img, img2):
+    img = img.convert("RGBA")
+    data1 = img.getdata()
+    img2 = img2.convert("RGBA")
+    data2 = img2.getdata()
+    newData = []
+    # data 1 is the layer below
+    # data 2 is the layer on top
+    i = 0
+    for item in data2:
+        if item[0] < 200 and item[1] < 200 and item[2] > 200:
+            newData.append(data1[i])
+        else:
+            newData.append(item)
+        i += 1
+    img.putdata(newData)
+    # img.show()
+    return img
 
-    # get masked background, mask must be inverted
-    mask = cv2.bitwise_not(mask)
-    bk_masked = cv2.bitwise_and(bk, bk, mask=mask)
-
-    # combine masked foreground and masked background
-    final = cv2.bitwise_or(fg_masked, bk_masked)
-    mask = cv2.bitwise_not(mask)  # revert mask to origina
-
-
-def generate_picture(path):
+#path: path to image's folder
+#txt: add-in text:
+def generate_picture(path, txt):
     images = [file for file in glob.glob(path)]
     images.sort()
-    print(images)
-
-    img0s   = Image.open(images[0])
-    w,h    = img0s.size
-
-    img0   = Image.new('RGB', (w,h))
-
-    #flag
+    #get image to work
+    img0   = Image.open(images[0])
     img1 = Image.open(images[1])
+    img2 = Image.open(images[-1])
+    img3 = Image.open(images[-2])
+    #img0.show()
+
+    #mid part:
     width, height = img1.size
     img1 = img1.resize((int(width / 3.6), int(height / 3.6)))
+    img0b = img0.copy()
     img0.paste(img1, (398, 165))
+    #img0.show()
+    img0 = layer_on_bw(img0b, img0)
+    #img0.show()
 
-    img2 = Image.open(images[3])
+    #top corner part
     width, height = img2.size
-    img2 = img2.resize((int(width / 3.5), int(height / 3.5)))
-    img0.paste(img2, (10, 12))
+    img2 = img2.resize((int(width / 3), int(height / 3)))
+    img2 = img2.crop((0, 8, int(width / 3), int(height / 3)))
+    img0.paste(img2, (10, 7))
+    img0 = layer_on_topcorner(img0b, img0)
+    #img0.show()
 
-    img0 = ImageChops.lighter(img0, img0s)
+    #profile picture part:
+    width, height = img3.size
+    draw = ImageDraw.Draw(img0)
+    draw.rectangle(((58, 435), (115, 435+115-58)), fill="black") # size: (57, 57)
+    #Add profile picture on black square
+    img3 = img3.resize((int(width / (width/57)), int(height / (height/57))))
+    img0.paste(img3, (58, 435))
+    #text part:
 
-    # Create a white rgba background
+    img0bt = img0.copy()
+    draw.rectangle(((200, 435), (750, 520)), fill="black")
+    fnt = ImageFont.truetype("arial.ttf", 22, encoding="unic")
+    draw.text((width/2 + 20, 440), txt[0], font=fnt, fill="Yellow")
+    #img0 = layer_on_bw(img0bt, img0)
+    line1 = "Which driver will win the 2019 Singapore F1?"
+    line2 = "Novi was among the " + " "*(len(txt[1])+2) + " who predicted " + " "*(len(txt[2])+2)
+    fnt1 = ImageFont.truetype("arial.ttf", 20, encoding="unic")
+    draw.text((width / 4 + 100, 470), line1, font=fnt1, fill="White")
+    draw.text((width / 4 + 50, 490), line2, font=fnt1, fill=("White"))
+
+    fnt2 = ImageFont.truetype("arial.ttf", 18, encoding="unic")
+    draw.text((width / 4 + 235, 492), txt[1], font=fnt2, fill=("Yellow"))
+    draw.text((width / 4 + 395, 492), txt[2], font=fnt2, fill=("Yellow"))
+
     img0.show()
-    #img0.save("result1.png")
-
-
-    # Saved in the same relative location
-    #img = img.convert("RGB")
-    #img.save("/home/lekhuong/Downloads/shit.jpg")
-
-'''
-dir = ""
-data_path = os.path.join(dir, '*g')
-'''
+    img0.save("finalresult.png")
 
 if __name__ == '__main__':
-    #generate_picture('/home/lekhuong/Downloads/*png')
-    delete_background('/home/lekhuong/Downloads/youpredict.png')
+    generate_picture(r'C:\Users\Khuong Le\Desktop\rs\*png', ["SINGAPORE FORMULA 1", "5%", "Lewis Hamilton."])
+    #layer_onto(r'C:\Users\Khuong Le\Desktop\rs\f1.png', r'C:\Users\Khuong Le\Desktop\rs\big.png')
